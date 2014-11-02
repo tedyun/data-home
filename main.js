7f1ed9c8c5725d95ab213bedbdca35f0fb6759f3
Ted Yun <ted.yun@oracle.com>
2014-11-01 21:26:19 -0400

    using svg

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-var data = [4, 8, 15, 16, 23, 42];
-var container = d3.select(".bodycontainer");
+var width = 420,
+    barHeight = 20;
 
-var barLinearScale = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+var x = d3.scale.linear()
+    .range([0, width]);
 
-container.append("div")
-  .attr("class", "barchart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return barLinearScale(d) + "px"; })
-    .text(function(d) { return d; });
+var chart = d3.select(".chart")
+    .attr("width", width);
+
+d3.tsv("data.tsv", type, function(error, data) {
+  x.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  chart.attr("height", barHeight * data.length);
+
+  var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d.value) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.value; });
+});
+
+function type(d) {
+  d.value = +d.value; // coerce to number
+  return d;
+}
